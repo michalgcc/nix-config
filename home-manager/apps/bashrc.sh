4263fc7582aea4bb27c53dfe186afbe3dc343363
Michal Gasz <gaszmichal@gmail.com>
2024-07-22 21:05:44 +0200

    feat: Add wsl_mnt_override logic

--- home-manager/apps/bashrc.sh
+++ home-manager/apps/bashrc.sh
@@ -1,6 +1,31 @@
 # Must be on start
 source "$(blesh-share)"/ble.sh --attach=none # does not work currently
 
+wsl_mnt_override="$HOME/.config/wsl_mnt_override/"
+
+link_wsl_exe() {
+    local windows_exe="$1"
+    local linux_exe="$2"
+
+    local windows_exe_path
+    windows_exe_path=$(type -a -P "$windows_exe")
+
+    if [[ -z "$windows_exe_path" ]]; then
+        echo "Error: $windows_exe not found"
+        return 1
+    fi
+
+    mkdir -p "$wsl_mnt_override"
+
+    local symlink_path="$wsl_mnt_override$(basename "$linux_exe")"
+
+    if [[ -L "$symlink_path" ]]; then
+        return 0
+    fi
+
+    ln -s "$windows_exe_path" "$symlink_path"
+}
+
 is_wsl() {
     grep -qi Microsoft /proc/version
 }
@@ -9,12 +34,33 @@ is_windows_filesystem() {
     pwd -P | grep -q "^/mnt/c/*"
 }
 
+prepend_path_if_not_found() {
+    if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
+        PATH="$1:$PATH"
+    fi
+}
+
+remove_path() {
+    PATH=$(echo "$PATH" | awk -v RS=: -v ORS=: '$0 != "'"$1"'"' | sed 's/:$//')
+}
+
 if is_wsl; then
     . ~/.nix-profile/etc/profile.d/nix.sh
+    link_wsl_exe "git.exe" "git"
+    link_wsl_exe "rg.exe" "rg"
 fi
 
-# PROMPT_COMMAND='PS1="$(powerline-go -newline -hostname-only-if-ssh)"; echo -ne "\033]0;$(pwd)\007"'
-PROMPT_COMMAND='PS1="$(powerline-go -hostname-only-if-ssh; [ -n "$container" ] && echo "📦 \n"; [ -z "$container" ] && echo "\n")"; echo -ne "\033]0;$(pwd)\007"'
+prepend_or_remove_wsl_mnt_override_path() {
+    if is_wsl; then
+        if is_windows_filesystem; then
+            prepend_path_if_not_found $wsl_mnt_override
+        else
+            remove_path $wsl_mnt_override
+        fi
+    fi
+}
+
+PROMPT_COMMAND='prepend_or_remove_wsl_mnt_override_path && PS1="$(powerline-go -hostname-only-if-ssh; [ -n "$container" ] && echo "📦 \n"; [ -z "$container" ] && echo "\n")"; echo -ne "\033]0;$(pwd)\007"'
 
 [ -z "$TMUX" ] && tmux new -A -s dflt
 
@@ -39,22 +85,6 @@ s() {
         explorer.exe "$(wslpath -aw "$@")"
     else
         echo "Not implemented. Implement when there is a use case."
-    fi
-}
-
-git() {
-    if is_windows_filesystem; then
-        git.exe "$@"
-    else
-        command git "$@"
-    fi
-}
-
-rg() {
-    if is_windows_filesystem; then
-        rg.exe "$@"
-    else
-        command rg "$@"
     fi
 }
 
